456387c7bda4284a3156a548a867c5636ba1128b
luisbelisario <luisbls77@hotmail.com>
2023-05-08 20:11:01 -0300

    first commit

---
+++ src/DivisionByZero.java
@@ -0,0 +1,22 @@
+public class DivisionByZero {
+
+    public static void main(String[] args) {
+        int controle = 1;
+        try {
+            for ( ; true; controle--) {
+                // Na primeira iteração controle = 1
+                // No if ocorre --controle que será zero
+                if((controle/--controle) <= 0)
+                    break;
+            }
+        } catch(ArithmeticException e) {
+            System.out.println("A");
+        } catch(RuntimeException e) {
+            System.out.println("B");
+        } catch(Exception e) {
+            System.out.println("C");
+        } finally {
+            System.out.println("D");
+        }
+    }
+}
---
+++ src/MDC.java
@@ -0,0 +1,19 @@
+public class MDC {
+
+    public static void main(String[] args) {
+        System.out.println(resolver(10,5));
+    }
+
+    public static int resolver(int a, int b) {
+        return a * (b / mdc(a, b));
+    }
+
+    private static int mdc(int a, int b){
+        while(b != 0){
+            int r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
+    }
+}
